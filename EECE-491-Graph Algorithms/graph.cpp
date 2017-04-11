94ac5afaa61879aa6fada2c7712dd0adab6f99b0
Nathan Wilk <nwilk01@manhattan.edu>
2017-04-11 12:59:12 -0400

    Testing everything but the searches

--- EECE-491-Graph Algorithms/graph.cpp
+++ EECE-491-Graph Algorithms/graph.cpp
@@ -3,7 +3,6 @@
 Graph::Graph()
 {
 	vertices = nullptr;
-	queue q;
 }
 // Graph()
 // Constructor initializes vertices linked list to empty
@@ -37,14 +36,15 @@ void Graph::AddVertex(string v)
 
 void Graph::AddEdge(string s, string d, int w)
 {
-	VertexNode *svertice = VertexExists(s);
+	VertexNode *svertex = VertexExists(s);
+	VertexNode *dvertex = VertexExists(d);
 	EdgeNode *newEdge = new EdgeNode;
 	EdgeNode *traverse = svertice->edgePtr;
 	if (svertice->edgePtr == nullptr)
 	{
-		svertice->edgePtr->destination = d;
-		svertice->edgePtr->weight = w;
-		svertice->edgePtr->nextEdge = nullptr;
+		svertex->edgePtr->destination = d;
+		svertex->edgePtr->weight = w;
+		svertex->edgePtr->nextEdge = nullptr;
 	}
 	else
 	{
@@ -52,7 +52,7 @@ void Graph::AddEdge(string s, string d, int w)
 		{
 			traverse = traverse->nextEdge;
 		}
-		newEdge->destination = d;
+		newEdge->destination = dvertex;
 		newEdge->weight = w;
 		newEdge->nextEdge = nullptr;
 		traverse->nextEdge = newEdge;
@@ -63,7 +63,7 @@ void Graph::AddEdge(string s, string d, int w)
 // Adds edge from source S  to destination D with specified weight W.
 // If there is not enough memory to add the edge, throw the GraphFull exception
 
-VertexNode* VertexExists(string v) const
+VertexNode* Graph::VertexExists(string v) const
 {
 	VertexNode *temp = vertices;
 	while (temp->vname != v || temp == nullptr)
@@ -79,3 +79,126 @@ VertexNode* VertexExists(string v) const
 // VertexExists()
 // Returns pointer to corresponding VertexNode if vertex V in graph 
 // Returns NULL otherwise
+
+
+EdgeNode* Graph::EdgeExists(string s, string d) const
+{
+	VertexNode *svertex = VertexExists(s);
+	VertexNode *dvertex = VertexExists(d);
+	EdgeNode *edge = vertex->edgePtr;
+	while (edge->destination != dvertex || edge != nullptr)
+	{
+		edge = edge->nextEdge;
+	}
+	if (edge == nullptr)
+	{
+		return NULL;
+	}
+	return edge;
+}
+// EdgeExists()
+// Returns pointer to edge node if edge from vertex s to vertex d exists in graph 
+// Returns NULL otherwise
+
+int  Graph::WeightIs(string s, string d)
+{
+	EdgeNode *temp = EdgeExists(s, d);
+	if (temp == NULL)
+	{
+		throw GraphEdgeNotFound();
+	}
+	return temp->weight;
+}
+// WeightIs()
+// Returns weight of edge (s,d).  Throws GraphEdgeNotFound if edge not present.
+
+void Graph::ClearMarks()
+{
+	VertexNode *traverse = vertices;
+	while (traverse->nextVertex != nullptr)
+	{
+		traverse->mark = false;
+		traverse = traverse->nextVertex;
+	}
+}
+// ClearMarks()
+// Clears all vertex marks
+
+void Graph::MarkVertex(string v)
+{
+	VertexNode *temp = VertexExists(v);
+	if (temp != NULL)
+	{
+		temp->mark = true;
+	}
+	else 
+	{
+		throw GraphVertexNotFound();
+	}
+}
+// MarkVertex()
+// Marks vertex V as visited
+// Throws GraphVertexNotFound if not present
+
+bool Graph::IsMarked(string v)
+{
+	VertexNode *temp= VertexExists(v);
+	if (temp != NULL)
+	{
+		if (temp->mark)
+		{
+			return true;
+		}
+		return false;
+	}
+	else
+	{
+		throw GraphVertexNotFound();
+	}
+}
+// IsMarked()
+// Returns true if vertex V is marked, false if not marked
+// Throws GraphVertexNotFound if not present
+
+void Graph::GetToVertices(string V, queue<string>& q)
+{
+	VertexNode *target = VertexExists(V);
+	EdgeNode *traverse = target->edgePtr;
+	if (target != NULL)
+	{
+		while (traverse != nullptr)
+		{
+			q.push(traverse->destination->vname);
+			traverse->nextEdge;
+		}
+	}
+}
+// GetToVertices()
+// Returns queue Q of vertex names of those vertices adjacent to vertex V
+// The queue here is from the Standard Template Library
+// Throws GraphVertexNotFound if not present
+
+void Graph::DepthFirstSearch(string startVertex, string endVertex, queue<string>& path)
+{
+
+}
+// DepthFirstSearch()
+// Notes:
+// (1) This algorithm is flawed in that as it searches for a path, it may
+// output some additional vertices that it visited but were not part
+// of the actual path.  
+// 
+// (2) This algorithm requires use of the stack and queue containers from the
+// Standard Template Library.  
+
+void Graph::BreadthFirstSearch(string startVertex, string endVertex, queue<string>& visitedq)
+{
+
+}
+// BreadthFirstSearch()
+// Uses the BFS algorithm to determine a path from the
+// startVertex to the endVertex.  If a path is found, the path vertices should
+// be in the visited queue.  If no path is found, the visited queue should be emptied
+// as a signal to the client code that no path exists between the start and
+// end vertices.
+
